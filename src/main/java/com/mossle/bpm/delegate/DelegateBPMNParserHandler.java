13b25bd5bb7fa25cab5f9ea4f4d673dee40e047e
myfeike <liuyuhua69@gmail.com>
2013-08-06 16:06:33 +0800

    candidate*** is supported

--- src/main/java/com/mossle/bpm/delegate/DelegateBPMNParserHandler.java
+++ src/main/java/com/mossle/bpm/delegate/DelegateBPMNParserHandler.java
@@ -11,19 +11,25 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class DelegateBPMNParserHandler extends UserTaskParseHandler {
-    private static Logger logger = LoggerFactory
-            .getLogger(DelegateBPMNParserHandler.class);
+	private static Logger logger = LoggerFactory.getLogger(DelegateBPMNParserHandler.class);
 
-    
-    protected void executeParse(BpmnParse bpmnParse, UserTask userTask) {
-    	 logger.info("bpmnParse : {}, userTask : {}", bpmnParse, userTask);
+	protected void executeParse(BpmnParse bpmnParse, UserTask userTask) {
+		logger.info("bpmnParse : {}, userTask : {}", bpmnParse, userTask);
 		super.executeParse(bpmnParse, userTask);
 		TaskDefinition taskDefinition = (TaskDefinition) bpmnParse.getCurrentActivity().getProperty(PROPERTY_TASK_DEFINITION);
+		
 		ActivitiListener activitiListener = new ActivitiListener();
 		activitiListener.setEvent(TaskListener.EVENTNAME_ASSIGNMENT);
 		activitiListener.setImplementationType(ImplementationType.IMPLEMENTATION_TYPE_DELEGATEEXPRESSION);
 		activitiListener.setImplementation("#{delegateTaskListener}");
 		taskDefinition.addTaskListener(TaskListener.EVENTNAME_ASSIGNMENT, bpmnParse.getListenerFactory().createDelegateExpressionTaskListener(activitiListener));
+
+		// candidateuser 或者group 包含被代理人  , 则添加代理人为candidateuser
+		ActivitiListener delegateActivitiCandidateListener = new ActivitiListener();
+		delegateActivitiCandidateListener.setEvent(TaskListener.EVENTNAME_CREATE);
+		delegateActivitiCandidateListener.setImplementationType(ImplementationType.IMPLEMENTATION_TYPE_DELEGATEEXPRESSION);
+		delegateActivitiCandidateListener.setImplementation("#{delegateTaskCreateListener}");
+		taskDefinition.addTaskListener(TaskListener.EVENTNAME_CREATE, bpmnParse.getListenerFactory().createDelegateExpressionTaskListener(delegateActivitiCandidateListener));
 	}
-    
+
 }

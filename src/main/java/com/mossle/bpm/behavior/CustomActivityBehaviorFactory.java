ed2d5c618126a1274e0a590802cae73283ae90f3
Xu Huisheng <xyz20003@gmail.com>
2013-08-08 19:22:43 +0800

    extends ActivityBehavior

---
+++ src/main/java/com/mossle/bpm/behavior/CustomActivityBehaviorFactory.java
@@ -0,0 +1,22 @@
+package com.mossle.bpm.behavior;
+
+import org.activiti.bpmn.model.UserTask;
+import org.activiti.engine.impl.bpmn.behavior.UserTaskActivityBehavior;
+import org.activiti.engine.impl.bpmn.parser.factory.DefaultActivityBehaviorFactory;
+import org.activiti.engine.impl.task.TaskDefinition;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.mossle.bpm.behavior.usertask.CustomUserTaskActivityBehavior;
+
+public class CustomActivityBehaviorFactory extends DefaultActivityBehaviorFactory {
+	private static Logger log = LoggerFactory.getLogger(CustomUserTaskActivityBehavior.class);
+	
+	//test
+	public UserTaskActivityBehavior createUserTaskActivityBehavior(UserTask userTask, TaskDefinition taskDefinition) {
+		log.info("change usertask Behavior : {}  ",userTask);
+		return new CustomUserTaskActivityBehavior(taskDefinition);
+	}
+	// 同样可以覆盖别的方法,加入其他元素的自定义行为,参考 @see ActivityBehaviorFactory
+	// 该类控制执行到某一元素时触发
+}
---
+++ src/main/java/com/mossle/bpm/parser/CustomBpmnParser.java
@@ -0,0 +1,15 @@
+package com.mossle.bpm.parser;
+
+import org.activiti.engine.impl.bpmn.parser.BpmnParser;
+import org.activiti.engine.impl.bpmn.parser.factory.ActivityBehaviorFactory;
+import org.activiti.engine.impl.bpmn.parser.factory.DefaultActivityBehaviorFactory;
+
+public class CustomBpmnParser extends BpmnParser{
+
+	public void setActivityBehaviorFactory(ActivityBehaviorFactory activityBehaviorFactory) {
+		// TODO Auto-generated method stub
+		((DefaultActivityBehaviorFactory)activityBehaviorFactory).setExpressionManager(expressionManager);
+		super.setActivityBehaviorFactory(activityBehaviorFactory);
+	}
+	
+}
